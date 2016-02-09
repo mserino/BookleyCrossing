cb5f51c08b3fe137874c20352a83b586e7836650
Serino, Margherita (UK - London) <mserino@deloitte.co.uk>
2016-02-09 09:41:10 +0000

    BC-007 borrow, return and unavailable logic around the button - WIP

--- client/scripts/books/book_item.js
+++ client/scripts/books/book_item.js
@@ -1,6 +1,19 @@
 Template.bookItem.helpers({
 	submitted: function() {
 		return this.submitted.toDateString();
+	},
+	borrowed: function() {
+		if(this.borrowedBy.username) {
+			return this.borrowedBy.username;
+		}
+	},
+	isBorrowing: function() {
+		if(this.borrowedBy.username) {
+			var borrowedBy = this.borrowedBy.username,
+				currentUser = Meteor.user().username;
+
+			return borrowedBy === currentUser;
+		}
 	}
 });
 
@@ -9,14 +22,10 @@ Template.bookItem.events({
 		e.preventDefault();
 
 		var book = Books.findOne({_id: this._id});
-		
-		Meteor.call('borrowBook', book, function(error, result) {
-			
-			if (error)
-				FlashMessages.sendError(error.reason, { autoHide: false });
-				console.log(error);
 
-			console.log('a', result);
+		Meteor.call('borrowBook', book, function(error, result) {
+			if (result.userNotLoggedIn)
+				FlashMessages.sendError('You need to be logged in to borrow a book', { autoHide: true, hideDelay: 5000 });
 		});
 	}
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -2,16 +2,22 @@ if (Books.find().count() === 0) {
 	Books.insert({
 		title: 'A thousand splendid suns',
 		author: 'Khaled Hosseini',
-		cover: 'http://ecx.images-amazon.com/images/I/713ZoQHz4mL.jpg'
+		cover: 'http://ecx.images-amazon.com/images/I/713ZoQHz4mL.jpg',
+		submitted: new Date(),
+		borrowedBy: ''
 	});
 	Books.insert({
 		title: 'Harry Potter and the Philosopers Stone',
 		author: 'Joanne K. Rowling',
-		cover: 'http://ecx.images-amazon.com/images/I/A1to2wtnOHL.jpg'
+		cover: 'http://ecx.images-amazon.com/images/I/A1to2wtnOHL.jpg',
+		submitted: new Date(),
+		borrowedBy: ''
 	});
 	Books.insert({
 		title: 'My name is Malala',
 		author: 'Malala Yousafzai',
-		cover: 'http://ecx.images-amazon.com/images/I/51Mn6IUJI%2BL.jpg'
+		cover: 'http://ecx.images-amazon.com/images/I/51Mn6IUJI%2BL.jpg',
+		submitted: new Date(),
+		borrowedBy: ''
 	});
 }
