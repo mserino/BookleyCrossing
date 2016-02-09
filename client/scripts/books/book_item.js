14636b37e56e4f30bde3b26ddcd9f6fdd41962b9
Serino, Margherita (UK - London) <mserino@deloitte.co.uk>
2016-02-09 11:50:29 +0000

    BC-007 user can only borrow one book at the time

--- client/scripts/books/book_item.js
+++ client/scripts/books/book_item.js
@@ -3,7 +3,7 @@ Template.bookItem.helpers({
 		return this.submitted.toDateString();
 	},
 	borrowed: function() {
-		if(this.borrowedBy.username) {
+		if (this.borrowedBy.username) {
 			return this.borrowedBy.username;
 		}
 	},
@@ -11,11 +11,11 @@ Template.bookItem.helpers({
 		return this.borrowedOn.toDateString();
 	},
 	isBorrowing: function() {
-		if(this.borrowedBy.username) {
+		if (this.borrowedBy.username) {
 			var borrowedBy = this.borrowedBy.username,
 				currentUser;
 
-			if(Meteor.user()) {
+			if (Meteor.user()) {
 				currentUser = Meteor.user().username;
 			}
 
@@ -31,14 +31,26 @@ Template.bookItem.events({
 		var book = Books.findOne({_id: this._id});
 
 		Meteor.call('borrowBook', book, function(error, result) {
-			if (result.userNotLoggedIn)
+
+			if (result.userNotLoggedIn) {
 				FlashMessages.sendError('You need to be logged in to borrow a book', { autoHide: true, hideDelay: 5000 });
+				Router.go('/sign-in');
+			}
+
+			if (result.userAlreadyBorrowing) {
+				$('.js-borrow-errors').text('Oops! You can\'t borrow more than one book at the time. Please return the book you\'re reading before borrowing a new one.');
+				FlashMessages.sendError('It looks like you are already borrowing a book. Please return it before borrowing a new one', { autoHide: true, hideDelay: 5000 });
+			} else {
+				FlashMessages.sendSuccess('Book successfully borrowed!', {autoHide: true, hideDelay: 5000});
+			}
 		});
 	},
 	'click .js-return-book': function(e) {
 		e.preventDefault();
 		var book = Books.findOne({_id: this._id});
 
-		Meteor.call('returnBook', book);
+		Meteor.call('returnBook', book, function(error, result) {
+			FlashMessages.sendSuccess('Thank you!', {autoHide: true, hideDelay: 5000});
+		});
 	}
 });
