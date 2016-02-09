918897580f080d37703bcf93dd329735d65198b8
Serino, Margherita (UK - London) <mserino@deloitte.co.uk>
2016-02-09 11:50:29 +0000

    BC-007 user can only borrow one book at the time

--- client/scripts/books/book_add.js
+++ client/scripts/books/book_add.js
@@ -10,15 +10,18 @@ Template.bookAdd.events({
 
 		//calling method in collections/books.js
 		Meteor.call('bookAdd', book, function(error, result) {
-			if (error)
+			if (error) {
 				FlashMessages.sendError(error.reason, { autoHide: false });
 				Router.go('bookAdd');
+			}
 
-			if (result.bookExists)
+			if (result.bookExists) {
 				FlashMessages.sendWarning('This book has already been posted', { autoHide: true, hideDelay: 5000 });
+			}
 
-			if (!result.bookExists)
+			if (!result.bookExists) {
 				FlashMessages.sendSuccess('Book successfully added to the bookshelf', {autoHide: true, hideDelay: 5000 });
+			}
 
 			Router.go('bookPage', {_id: result._id});
 		});
