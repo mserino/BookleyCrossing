0b4c3252597193da2dfaf378487eac227dceb80a
Serino, Margherita (UK - London) <mserino@deloitte.co.uk>
2016-02-09 08:10:22 +0000

    BC-007 user can borrow book and save it to database - WIP

--- server/accounts.js
+++ server/accounts.js
@@ -1,12 +1,11 @@
 Accounts.onCreateUser(function(options, user) {
   user.borrowing = '';
-	
 	if (options.profile)
 		user.profile = options.profile;
-  
+
   return user;
 });
 
-Meteor.publish('userData', function() { 
+Meteor.publish('userData', function() {
   return Meteor.users.find();
 });
