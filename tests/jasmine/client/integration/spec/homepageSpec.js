2483ccd5dd58878f7d4148a297a450575a21995a
Pellicena Tredway, Nicole (UK - London) <npellicenatredway@deloitte.co.uk>
2016-02-14 01:19:26 +0000

    fixes database testing issues, adds path for dashboard in routes

--- tests/jasmine/client/integration/spec/homepageSpec.js
+++ tests/jasmine/client/integration/spec/homepageSpec.js
@@ -1,38 +1,41 @@
 describe("homepage", function() {
 
-    beforeEach(function (done) {
-      Meteor.loginWithPassword("test@test.com", "testing", function(err) {
-        Router.go('/');
-        Tracker.afterFlush(done);
-      });
-    });
+  beforeEach(function (done) {
+    Router.go('/');
+    Tracker.afterFlush(done);
+  });
 
-    // beforeEach(waitForRouter);
+  beforeEach(waitForRouter);
 
-    afterEach(function(done){
-      Meteor.logout(function() {
-        done();
-      });
-    });
+  describe("layout", function() {
 
-  describe("header", function(){
-
-    describe("when a user is logged in", function(){
-      // it("a link is displayed for the dashboard ", function() {
-        // expect(Meteor.user()).toBeNull();
-        // expect(Meteor.userId()).not.toBeNull()
-        // var profile = $(".header-login__link:contains(My profile)");
-        // expect(profile.text()).toEqual("My profile");
-        // console.log(profile);
-      // });
+    it("displays the latest additions heading", function(){
+      expect($('h2').text()).toEqual('Latest additions View all books');
     });
 
   });
 
-  describe("latest additions", function(){
-    it("shows 'Latest additions' heading", function(){
-      expect($('h2').text()).toEqual('Latest additions View all books');
+  describe("header", function() {
+
+    describe("when a user is logged in", function() {
+      it("a link is displayed for the dashboard", function(done) {
+        expect(Meteor.userId()).toBeNull();
+
+        Meteor.loginWithPassword("test@test.com", "testtest", function(err) {
+          expect(err).toBeUndefined();
+          expect(Meteor.userId()).not.toBeNull();
+          // var profile = $(".header-login__link:contains('My profile')");
+          // expect(profile.text()).toEqual("My profile");
+          // console.log($(".header-login__link"));
+
+          Meteor.logout(function() {
+            done();
+          });
+
+        });
+      });
     });
+
   });
 
 });
