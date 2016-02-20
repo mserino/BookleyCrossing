8e734a5036fa78b3a1223ff049c4997929d6cab2
Margherita Serino <m.serino@hotmail.it>
2016-02-20 13:06:47 +0000

    BC-028 user can request a book, one at a time, only if logged in

--- server/fixtures.js
+++ server/fixtures.js
@@ -30,7 +30,8 @@ if (Books.find().count() === 0) {
 		cover: 'http://ecx.images-amazon.com/images/I/713ZoQHz4mL.jpg',
 		submitted: new Date(),
 		borrowedBy: test,
-		borrowedOn: new Date('February 8, 2016 03:24:00')
+		borrowedOn: new Date('February 8, 2016 03:24:00'),
+		requestedBy: ''
 	});
 	Books.insert({
 		title: 'Harry Potter and the Philosopers Stone',
@@ -38,7 +39,8 @@ if (Books.find().count() === 0) {
 		cover: 'http://ecx.images-amazon.com/images/I/A1to2wtnOHL.jpg',
 		submitted: new Date(),
 		borrowedBy: '',
-		borrowedOn: ''
+		borrowedOn: '',
+		requestedBy: ''
 	});
 	Books.insert({
 		title: 'My name is Malala',
@@ -46,7 +48,8 @@ if (Books.find().count() === 0) {
 		cover: 'http://ecx.images-amazon.com/images/I/51Mn6IUJI%2BL.jpg',
 		submitted: new Date(),
 		borrowedBy: '',
-		borrowedOn: ''
+		borrowedOn: '',
+		requestedBy: ''
 	});
 	Books.insert({
 		title: 'The Japanese Lover',
@@ -54,7 +57,8 @@ if (Books.find().count() === 0) {
 		cover: 'http://ecx.images-amazon.com/images/I/41oGAdM1sOL.jpg',
 		submitted: new Date('January 1, 2016 03:24:00'),
 		borrowedBy: '',
-		borrowedOn: ''
+		borrowedOn: '',
+		requestedBy: ''
 	});
 	Books.insert({
 		title: 'Norwegian Wood',
@@ -62,7 +66,8 @@ if (Books.find().count() === 0) {
 		cover: 'http://ecx.images-amazon.com/images/I/41rw1DHW2FL.jpg',
 		submitted: new Date(),
 		borrowedBy: '',
-		borrowedOn: ''
+		borrowedOn: '',
+		requestedBy: ''
 	});
 	Books.insert({
 		title: 'All the light we cannot see',
@@ -70,6 +75,7 @@ if (Books.find().count() === 0) {
 		cover: 'http://ecx.images-amazon.com/images/I/511qpsWcTyL.jpg',
 		submitted: new Date('January 7, 2016 00:00:00'),
 		borrowedBy: '',
-		borrowedOn: ''
+		borrowedOn: '',
+		requestedBy: ''
 	});
 }
